955205123aacd7d2e804a0d5a23fd9fb4fa82331
Lucas Reis <lucas.reis@strategymanager.com.br>
2016-11-10 21:57:28 -0200

    não ta pronto

--- jquery/soma-total.js
+++ jquery/soma-total.js
@@ -1,4 +1,19 @@
 
+var removeItem = function(){
+    event.preventDefault();
+
+    var eu = $(this);
+
+    var itemTotal = eu.closest("tr").find(".item-total");
+    var preco = parseFloat(itemTotal.text());
+    var precoFinal -=
+    eu.closest("tr").remove();
+
+    var quantidade = parseInt($("#quantidade-itens").text());
+    var quantidadeAtualizada = quantidade - 1;
+    $("#quantidade-itens").text(quantidadeAtualizada);
+}
+
 var aposInicializado = function(){
     var itens = $(".item-total");
     var total = 0;
@@ -12,6 +27,7 @@ var aposInicializado = function(){
 
     $("#valor-total").text(total);
     $("#quantidade-itens").text(itens.length);
+    $(".remove-item").click(removeItem);
 };
 
 $(aposInicializado);
