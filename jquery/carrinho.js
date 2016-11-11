41cabd0126d43f3739fcf7f1850d67bdd821f818
Lucas Reis <lucas.reis@strategymanager.com.br>
2016-11-11 18:30:11 -0200

    deixando o js mais dinamico

--- jquery/carrinho.js
+++ jquery/carrinho.js
@@ -1,16 +1,24 @@
 
 var atualizaDados = function(){
-    var items = $(".item-total:visible");
-    var total = 0;
+    var carrinhos = $(".carrinho");//seleciona todos os elementos com a classe carrinho
 
-    for(var i=0; i < items.length; i++) {
-        var conteudo = $(items[i]).text();
-        var preco = parseFloat(conteudo);
-        total += preco;
-    }
-    $("#valor-total").text(total);
-    $("#quantidade-itens").text(items.length);
+    //para cada elemento, atualizar os dados
+    carrinhos.each(function(){
+        var carrinho = $(this);
+        var items = carrinho.find(".item-total:visible");//procurar dentro dos elementos de carrinho os elemetnos com a classe item-total setadas como visible
+        var total = 0;
+
+        for(var i=0; i < items.length; i++) {
+            var conteudo = $(items[i]).text();
+            var preco = parseFloat(conteudo);
+            total += preco;
+        }
+        carrinho.find(".valor-total").text(total);
+        carrinho.find(".quantidade-itens").text(items.length);
+    });
 };
+
+
 //a diferenca está em que, toda vez que se remove um item se faz o caluclo do total novamente atualizando os dados
 var removeItem = function() {
     event.preventDefault();
@@ -22,18 +30,21 @@ var removeItem = function() {
 };
 
 var undo = function(){
-    $("tr:visible").removeClass("recuperado");
+    var carrinho = $(this).closest(".carrinho");
+    carrinho.find("tr:visible").removeClass("recuperado");
 
-    var escondido = $("tr:hidden"); //seleciona todos os trs que possuem como display hide
+    var escondido = carrinho.find("tr:hidden"); //seleciona todos os trs que possuem como display hide
     escondido.addClass("recuperado");
     escondido.show();
+
+    atualizaDados();
 };
 
 
 var aposInicializado = function() {
 
     $(".remove-item").click(removeItem);
-    $("#undo").click(undo);
+    $(".undo").click(undo);
     atualizaDados();
 };
 
