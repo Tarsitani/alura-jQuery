4e50aea40977ff1be1576da7205df2cd112ff888
Lucas Reis <lucas.reis@strategymanager.com.br>
2016-11-14 14:07:10 -0200

    A wild advertising appears!

--- jquery/carrinho.js
+++ jquery/carrinho.js
@@ -1,3 +1,22 @@
+var propaganda = function(){
+
+    var propagandas = ["Que tal comprar mais coisas? Sim, sou uma propaganda hu3",
+                        "Seria uma pena se nao tivesse propaganda... Tcharam",
+                        "Olha a propaganda aqui de novo ^.^",
+                        "Alo... alo... propaganda?? Aqui estamos!!! :D"];
+    var posicao = Math.floor(propagandas.length * Math.random()); //seleciona um numero aleatório de posicao até o tamanho maximo do array e arredona para baixo com Math.floor
+    var texto = propagandas[posicao]; //salva o texto aleatorio a nessa variavel
+    var tr = $("<tr>").addClass("propaganda").append($("<td>")); //cria um tr com a classe propaganda e uma tag td
+    tr.find("td").attr("colspan", 6); //procura a tag td dentro do tr e atribui o atributo colspan + seu valor
+    tr.find("td").text(texto);//na variavel tr procura o a tag td e adiciona como texto a variavel texto dava pra fazer na linha de cima... :D
+    return tr;//retorna a tr criada com todos os seus elementos
+
+    //um outro jeito seria criar tudo de uma vez concateando, mas ai depende do caso pra utilizar esse metodo
+    //var propaganda = propagandas[Math.floor(propagandas.length * Math.random())];
+    //var tr = $("<tr class='propaganda'><td colspan='6'>" + proapganda + "</td></tr>"); //tryhardando
+    //return tr;
+
+};
 
 var atualizaDados = function(){
     var carrinhos = $(".carrinho");//seleciona todos os elementos com a classe carrinho
@@ -46,6 +65,16 @@ var aposInicializado = function() {
     $(".remove-item").click(removeItem);
     $(".undo").click(undo);
     atualizaDados();
+    //para cada elemento que possui a classe carrinho
+    $(".carrinho").each(function(){
+        //procura a cada 'enesimo' elemento filho, nesse caso terceiro(nth-child(3n)) e para cada chama uma funcao. Separado por virgula colocamos mais um caso, sempre no ultimo elemento chama a fucntion tb
+        $(this).find("tr:nth-child(3n), tr:last").each(function(){
+            propaganda().insertAfter($(this));//vai inserir a propaganda depois do atual elemento (tb existe insertBefore)
+
+        });
+
+    });
+
 };
 
 $(aposInicializado);
